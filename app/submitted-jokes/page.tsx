d9d2fbfbbe2b3f03671881b7dbdaf98cd10279b0
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2024-07-21 04:14:12 +0530

    Update page layout for Submitted Jokes

--- app/submitted-jokes/page.tsx
+++ app/submitted-jokes/page.tsx
@@ -2,7 +2,7 @@
 // ! Moderator must be logged in to view this page
 export default function SubmittedJokes() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="mt-4">
       <h1 className="text-4xl font-bold">Submitted Jokes</h1>
 
       {/* request a new joke from "Submit Jokes" microservice */}
