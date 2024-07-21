2d2c3b9f7e7cb3bcc68ef2ac8311c3b17c82eea0
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2024-07-21 19:27:02 +0530

    Refactor SubmittedJokes page to include a Back to Home button

--- app/submitted-jokes/page.tsx
+++ app/submitted-jokes/page.tsx
@@ -1,8 +1,18 @@
 // Show a list of jokes that the users have submitted
+
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 // ! Moderator must be logged in to view this page
 export default function SubmittedJokes() {
   return (
-    <main className="mt-4">
+    <main className="flex min-h-screen flex-col items-center justify-center pt-5">
+      <Link href="/">
+        <Button variant={"secondary"} className="absolute top-5 left-5">
+          Back to Home
+        </Button>
+      </Link>
+
       <h1 className="text-4xl font-bold">Submitted Jokes</h1>
 
       {/* request a new joke from "Submit Jokes" microservice */}
