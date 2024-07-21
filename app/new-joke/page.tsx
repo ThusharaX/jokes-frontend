80921f0d5a1ee48c629c0858ba6a25fc526e9bab
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2024-07-21 19:26:52 +0530

    Refactor NewJoke page to include a Back to Home button

--- app/new-joke/page.tsx
+++ app/new-joke/page.tsx
@@ -1,10 +1,17 @@
 "use client";
 import InsertJokeForm from "@/app/_components/new-joke/InsertJokeForm";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 //
 export default function NewJoke() {
   //
   return (
     <main className="flex min-h-screen flex-col justify-center pt-5">
+      <Link href="/">
+        <Button variant={"secondary"} className="absolute top-5 left-5">
+          Back to Home
+        </Button>
+      </Link>
       <InsertJokeForm />
     </main>
   );
