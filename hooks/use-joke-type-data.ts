5f1bc43833a6c7155e6b71aa9cfa35d05b2d6baf
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2024-07-21 19:37:22 +0530

    Refactor API URLs to remove version number

--- hooks/use-joke-type-data.ts
+++ hooks/use-joke-type-data.ts
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 //
-const baseURL = "http://localhost:3001/api/v1";
+const baseURL = "http://localhost:3001";
 //
 const fetchJokeTypes = async () => {
   const { data } = await axios.get(`${baseURL}/joke-types`);
