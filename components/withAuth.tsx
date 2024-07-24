b4f4d0b1bfe2efe14592df0bf098afbe3813ed76
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2024-07-25 03:59:47 +0530

    fix: authentication mistake fixed

---
+++ components/withAuth.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useAuthStore } from "@/store/useAuthStore";
+import { useEffect } from "react";
+import { redirect } from "next/navigation";
+
+export default function withAuth(Component: any) {
+  return function WithAuth(props: any) {
+    const { isAuthenticated } = useAuthStore((state) => ({
+      isAuthenticated: state.isAuthenticated,
+    }));
+
+    useEffect(() => {
+      if (!isAuthenticated) {
+        redirect("/login");
+      }
+    }, []);
+
+    if (!isAuthenticated) {
+      return null;
+    }
+
+    return <Component {...props} />;
+  };
+}
