885684fb0fe50f36b1b2d2ab9b40bec2bdaecd13
Thushara Thiwanka <thusharathiwanka01@gmail.com>
2024-07-25 11:52:50 +0530

    refactor: Update withAuth hook to include isAuthenticated dependency

--- components/withAuth.tsx
+++ components/withAuth.tsx
@@ -14,7 +14,7 @@ export default function withAuth(Component: any) {
       if (!isAuthenticated) {
         redirect("/login");
       }
-    }, []);
+    }, [isAuthenticated]);
 
     if (!isAuthenticated) {
       return null;
